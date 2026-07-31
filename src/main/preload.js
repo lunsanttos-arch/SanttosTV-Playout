8019db11035fc7aab76f9e04a804da91f417aed0
lunsanttos-arch <lunsanttos@gmail.com>
2026-07-31 05:09:31 -0300

    Expand santtosAPI with media management functions

--- src/main/preload.js
+++ src/main/preload.js
@@ -3,6 +3,29 @@ const {
     ipcRenderer
 } = require("electron");
 
-contextBridge.exposeInMainWorld("santtosAPI", {
-    selectVideos: () => ipcRenderer.invoke("select-video")
-});
+contextBridge.exposeInMainWorld(
+    "santtosAPI",
+    {
+        selectVideos: () =>
+            ipcRenderer.invoke(
+                "media:select"
+            ),
+
+        getMedia: () =>
+            ipcRenderer.invoke(
+                "media:list"
+            ),
+
+        importMedia: (filePaths) =>
+            ipcRenderer.invoke(
+                "media:import",
+                filePaths
+            ),
+
+        removeMedia: (mediaId) =>
+            ipcRenderer.invoke(
+                "media:remove",
+                mediaId
+            )
+    }
+);
