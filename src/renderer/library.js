b7678d30f31403f07c527b61d8f95fcf1cc002e4
lunsanttos-arch <lunsanttos@gmail.com>
2026-07-31 04:20:15 -0300

    Add click event listener to add-media button

---
+++ src/renderer/library.js
@@ -0,0 +1,19 @@
+const button =
+document.getElementById(
+    "add-media"
+);
+
+
+
+button.addEventListener(
+    "click",
+    ()=>{
+
+
+        alert(
+            "Selecionar vídeo em breve..."
+        );
+
+
+    }
+);
